05622a30714a33ec64c75a9df41fc86c00755161
sandibi13 <sandipanb223@gmail.com>
2024-05-15 21:06:09 +0530

    added and configured tailwind

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,32 +10,9 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1 className="text-3xl text-center text-violet-500">
+        Hello World!
+      </h1>
     </div>
   );
 }
