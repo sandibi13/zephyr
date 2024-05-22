60dea4ec0ca48c556c4cd11ebdbda52e53b63acd
sandibi13 <sandipanb223@gmail.com>
2024-05-22 14:11:39 +0530

    fix: navbar's border styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <nav className="sticky inset-x-0 top-0 z-30 h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all dark:border-gray-700 dark:bg-gray-900/75">
       <WidthWrapper>
-        <div className="flex h-14 items-center justify-between">
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200 dark:border-zinc-700">
           <Link to="/" className="z-40 flex items-center space-x-1">
             <img
               src="/app/assets/logo.png"
