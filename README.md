70857dd884239c67ea736cefd16e5a93796ad6bb
sandibi13 <sandipanb223@gmail.com>
2024-06-10 10:32:02 +0530

    cleaned the repo

--- README.md
+++
@@ -1,7 +0,0 @@
-# PagePal
-
-**PagePal** is an A.I. powered PDF summarizer built with Remix.js.
-
-## Status
-
-This project is still under development. Stay tuned for updates and new features!
--- app/routes/kinde-auth.$index.ts
+++
@@ -1,6 +0,0 @@
-import { handleAuth } from "@kinde-oss/kinde-remix-sdk";
-import { LoaderFunctionArgs } from "@remix-run/node";
-
-export async function loader({ params, request }: LoaderFunctionArgs) {
-  return await handleAuth(request, params.index);
-}
