f1876ff2c2eaf7e28405bf46036dc7725e4a4ed1
sandibi13 <sandipanb223@gmail.com>
2024-07-13 11:26:50 +0530

    added feedback page

---
+++ src/app/(main)/feedback/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Feedback } from "~/components/feedback";
+
+export default function FeedbackPage() {
+  return (
+    <div className="grid min-h-screen place-items-center p-4">
+      <Feedback />
+    </div>
+  );
+}
