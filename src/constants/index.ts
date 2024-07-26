8d36c8ba25797523d9b9e6bf2b61962be3a862e8
sandibi13 <sandipanb223@gmail.com>
2024-07-26 18:45:10 +0530

    changed email-sender domain

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,7 +8,7 @@ export const links = [
 
 export const twitterUrl = "https://twitter.com/sandibi13";
 export const githubUrl = "https://github.com/sandibi13/zephyr";
-export const EMAIL_SENDER = "'Zephyr' <noreply@example.com>";
+export const EMAIL_SENDER = "'Zephyr' <onboarding@resend.dev>";
 
 export enum Paths {
   Home = "/",
